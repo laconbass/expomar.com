01f3138b74dd6e6f66a26b51aa19b44ac0a3a669
Lorenzo García Rivera <lorenzogrv@gmail.com>
2014-05-05 22:48:34 +0200

    customized log output

--- backend/app-main.js
+++ backend/app-main.js
@@ -6,6 +6,8 @@ var iai = require('../../iai')
   , marked = require( 'marked' )
   , readFile = require( 'fs' ).readFile
   , vhost = iai( 'middleware/vhost' )
+  , morgan = require( 'morgan' )
+  , bytes = require( 'bytes' )
 ;
 
 module.exports = app;
@@ -31,6 +33,29 @@ app.engine( 'md', function( path, locals, callback ){
 app.set( 'views', project.resolve('templates') );
 app.set( 'view engine', 'html' );
 
+morgan.format( 'custom', function(tokens, req, res){
+  var status = res.statusCode
+    , len = parseInt(res.getHeader('Content-Length'), 10)
+    , color = 32;
+
+  if (status >= 500) color = 31
+  else if (status >= 400) color = 33
+  else if (status >= 300) color = 36;
+
+  len = isNaN(len)
+    ? ''
+    : len = ' - ' + bytes(len);
+
+  return '' + morgan['remote-addr']( req )
+    + ' \x1b[90m' + req.method
+    + ' ' + req.host + (req.originalUrl || req.url) + ' '
+    + '\x1b[' + color + 'm' + res.statusCode
+    + ' \x1b[90m'
+    + (new Date - req._startTime)
+    + 'ms' + len
+    + '\x1b[0m';
+});
+
 //
 // Middleware & subdomain/path routing
 //
@@ -38,7 +63,7 @@ app.set( 'view engine', 'html' );
 app
   .use( require( 'static-favicon' )( project.resolve('static/favicon.ico') ) )
   .use( vhost( 'static.*', project.require('backend/app-static') ) )
-  .use( require( 'morgan' )( iai.production? ':remote-addr - :method :req[Host]:url - :status - :res[content-length] - :response-time ms' : 'dev' ) )
+  .use( morgan( iai.production? 'custom' : 'dev' ) )
   .use( vhost( 'admin.*', project.require('backend/app-admin') ) )
   .use( project.require('backend/app-public') )
 ;
