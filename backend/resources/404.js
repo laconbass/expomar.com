6654c6e622f12e2ca90217958c0bf8fd8286945c
Lorenzo García Rivera <lorenzogrv@gmail.com>
2014-05-14 19:42:36 +0200

    cleanup old resources folder

--- backend/resources/404.js
+++
@@ -1,7 +0,0 @@
-var iai = require('iai');
-
-module.exports = iai.item( __filename )
-  .name( 'Page Not Found' )
-  .layout( 'templates/404.swig.html' )
-  .styles( 'styles/404.less' )
-;
--- backend/resources/500.js
+++
@@ -1,6 +0,0 @@
-var iai = require('iai');
-
-module.exports = iai.item( __filename )
-  .name( 'Internal Server Error' )
-  .layout( 'templates/500.swig.html' )
-;
