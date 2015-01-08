45fb114c3d01d1009db8a4e3200d98c4fc2bd6d1
Lorenzo García Rivera <lorenzogrv@gmail.com>
2015-01-08 21:53:00 +0100

    empregar só un menu de navegacion

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
   $('#messages > *').click(function(){ $(this).remove(); });
+  var menu = $('#menu-top');
+  $('#menu-top button').click(function(){
+    menu.toggleClass('expanded');
+  });
 })
